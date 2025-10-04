de227df7695bd3bb2fe8acc2a309d12f7621a369
Nicolai Frigaard <46283121+jovialen@users.noreply.github.com>
2025-10-04 10:19:13 +0200

    feat: add login page

--- lib/core/auth/login.dart
+++ lib/core/auth/login.dart
@@ -1,11 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:metter/config/routes/router.dart';
+import 'package:metter/core/auth/helpers/validators.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage
-({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user != null) {
+        MetterRouter.router.pop(context);
+      }
+    });
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => {MetterRouter.router.pop(context)},
+          icon: Icon(LucideIcons.chevronLeft),
+        ),
+        title: Text("Log in"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _emailController,
+                    validator: emailValidator,
+                    decoration: InputDecoration(label: Text("Email")),
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(label: Text("Password")),
+                  ),
+                  TextButton(
+                    onPressed: () => submit(context),
+                    child: Text("Register"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void submit(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (exception) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(exception.toString())));
+    }
   }
 }
